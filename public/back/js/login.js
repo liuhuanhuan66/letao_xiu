b2b9d80bd40a1a0153222f1118ed932f82a9503e
liuhuanhuan66 <1217347384@qq.com>
2018-06-18 22:01:04 +0800

    项目修改

---
+++ public/back/js/login.js
@@ -0,0 +1,25 @@
+$(function(){
+  $("form").bootstrapValidator({
+    //校验用户名，对应name表单的name属性
+    username: {
+      validators: {
+        //不能为空
+        notEmpty: {
+          message: '用户名不能为空'
+        },
+        //长度校验
+        stringLength: {
+          min: 6,
+          max: 30,
+          message: '用户名长度必须在6到30之间'
+        },
+        //正则校验
+        regexp: {
+          regexp: /^[a-zA-Z0-9_\.]+$/,
+          message: '用户名由数字字母下划线和.组成'
+        }
+      }
+    },  
+  })
+});
+
